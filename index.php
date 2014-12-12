4eff8f9e0f83934571cd9655060d6676ba82bf63
Amanda Regan <regan008@cougars.csusm.edu>
2014-12-12 12:42:30 -0500

    2nd attempt at adding header to post page

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php get_header(); ?>
-
+<div class="homebanner">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2 home">
+        <h1>Digital History Portfolio</h1>
+        <h2>Amanda Regan</h2>
+      </div>
+    </div><!-- end .row-->
+  </div> <!-- end .container-->
+</div> <!-- end #banner-->
 
     <div class="container">
 
--- page.php
+++ page.php
@@ -5,16 +5,7 @@ Template Name: Page - Right Sidebar
 ?>
 
 <?php get_header(); ?>
-      <div class="homebanner">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-8 col-md-offset-2 home">
-        <h1>Digital History Portfolio</h1>
-        <h2>Amanda Regan</h2>
-      </div>
-    </div><!-- end .row-->
-  </div> <!-- end .container-->
-</div> <!-- end #banner-->
+      
       <div class="container">
 
         <div id="content" class="clearfix row">
